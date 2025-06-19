8787981fb012e97e3417e0ae32051bb24e3db50e
d8a928b2 <dattranvnu@outlook.com>
2025-06-19 10:34:00 +0000

    Add starting files

---
+++ lessons/bai1_1/solution.ts
@@ -0,0 +1,13 @@
+
+export type Block = {
+  index: number;
+  timestamp: string;
+  transactions: any[];
+  previous_hash: string;
+  current_hash: string;
+};
+
+// ✍️ TODO: Viết hàm tại đây
+export function isValidBlock(block: Block): boolean {
+  return false; // Chỉnh lại logic
+}
