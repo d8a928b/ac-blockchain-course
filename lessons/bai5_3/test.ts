f5cf69b62b2134f34a0e264f8e18ed414bb185cf
d8a928b <dattranvnu@outlook.com>
2025-07-09 12:40:34 +0000

    Update bai5_3

--- lessons/bai5_3/test.ts
+++ lessons/bai5_3/test.ts
@@ -7,7 +7,7 @@ async function main() {
     "function getCount() public view returns (uint)",
     "function increment() public"
   ];
-  const contractAddress = "0x41d0Ad4E6227062B59CA673B418Fc00E3A2d10Cf"; // Replace with your contract address
+  const contractAddress = "0x102Aa57dc9F8E1Ab2978604ea5C98b6509b5021e"; // Replace with your contract address
 
   const contract = new ethers.Contract(contractAddress, abi, provider);
 
