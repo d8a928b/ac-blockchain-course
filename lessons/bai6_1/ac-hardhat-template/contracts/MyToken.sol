01cbf659b8baf6bb1a45e17645abe464653ef6af
d8a928b <dattranvnu@outlook.com>
2025-07-10 01:04:21 +0000

    Update bai6_1

---
+++ lessons/bai6_1/ac-hardhat-template/contracts/MyToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    // Tên token: MyToken Symbol: MTK
+    constructor() ERC20("MyToken", "MTK") {
+        // Tổng cung: 1,000,000 token
+        // Mint toàn bộ cho deployer trong constructor
+        _mint(msg.sender, 1_000_000 * 10**decimals());
+    }
+}
