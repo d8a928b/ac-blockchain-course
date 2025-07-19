6d4a714409c699dfda1c0cb390b7bc066c72ab50
d8a928b <dattranvnu@outlook.com>
2025-07-19 06:03:16 +0000

    Update bai6_3

--- lessons/bai6_3/MyNFT/deploy/1-deploy.ts
+++ lessons/bai6_3/MyNFT/deploy/1-deploy.ts
@@ -1,7 +1,9 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
-const deployMyNFT: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployMyNFT: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+) {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -10,8 +12,7 @@ const deployMyNFT: DeployFunction = async function (hre: HardhatRuntimeEnvironme
     from: deployer,
     args: [deployer], // truyền initialOwner vào constructor
     log: true,
-    });
-
+  });
 };
 
 export default deployMyNFT;
